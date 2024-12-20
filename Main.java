6216843eed1cd097197c73497ae217c7a91c4162
Михаил Маранин <92624898+mishanya489@users.noreply.github.com>
2024-12-20 14:50:46 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,69 +6,58 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
-            String input;
-            while ((input = bufferedReader.readLine()) != null) {
-                System.out.println(calc(input));
-            }
+            System.out.println(calc(bufferedReader.readLine()));
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
     }
 
     public static String calc(String input) {
-        String result = null;
-        try {
-            String[] elements = input.split(" ");
+        String[] elements = input.split(" ");
 
-            Exception exception = checkInput(elements);
-            if (exception != null) {
-                throw exception;
-            }
+        checkInput(elements);
 
-            int num1 = Integer.parseInt(elements[0]);
-            int num2 = Integer.parseInt(elements[2]);
-            String operation = elements[1];
+        int num1 = Integer.parseInt(elements[0]);
+        int num2 = Integer.parseInt(elements[2]);
+        String operation = elements[1];
 
-
-            switch (operation) {
-                case "+":
-                    num1 += num2;
-                    break;
-                case "-":
-                    num1 -= num2;
-                    break;
-                case "*":
-                    num1 *= num2;
-                    break;
-                case "/":
-                    num1 /= num2;
-                    break;
-            }
-
-            result = String.valueOf(num1);
-
-        } catch (Exception e) {
-            result = e.toString();
+        switch (operation) {
+            case "+":
+                num1 += num2;
+                break;
+            case "-":
+                num1 -= num2;
+                break;
+            case "*":
+                num1 *= num2;
+                break;
+            case "/":
+                num1 /= num2;
+                break;
         }
-        return result;
+        return String.valueOf(num1);
     }
 
-    private static Exception checkInput(String[] elements) {
-        if (elements.length != 3) {
-            return new Exception("Неправильное количество введенных элементов!");
-        }
+    private static void checkInput(String[] elements) {
+        try {
+            if (elements.length != 3) {
+                throw new Exception("Неправильное количество введенных элементов!");
+            }
 
-        double num1 = Integer.parseInt(elements[0]);
-        double num2 = Integer.parseInt(elements[2]);
+            double num1 = Integer.parseInt(elements[0]);
+            double num2 = Integer.parseInt(elements[2]);
 
-        if ((num1 < 1 || num1 > 10) || (num2 < 1 || num2 > 10)) {
-            return new Exception("Неправильное значение одного из чисел!");
-        }
+            if ((num1 < 1 || num1 > 10) || (num2 < 1 || num2 > 10)) {
+                throw new Exception("Неправильное значение одного из чисел!");
+            }
 
-        ArrayList<String> operations = new ArrayList<>(List.of("+", "-", "*", "/"));
-        if (!operations.contains(elements[1])) {
-            return new Exception("Неправильная операция!");
+            ArrayList<String> operations = new ArrayList<>(List.of("+", "-", "*", "/"));
+            if (!operations.contains(elements[1])) {
+                throw new Exception("Неправильная операция!");
+            }
+        } catch (Exception e) {
+            System.err.println(e.fillInStackTrace());
+            System.exit(1);
         }
-        return null;
     }
 }
